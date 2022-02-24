f4b016c6aa636f0346557ab46fbe2dcc41c5f943
Anusha <anushavenu1997@gmail.com>
2022-02-24 17:58:16 +0530

    updated

--- assignment10/DOM-3/todo.js
+++ assignment10/DOM-3/todo.js
@@ -17,23 +17,6 @@ function newElement() {
     span.className = "close";
     span.appendChild(txt);
     li.appendChild(span);
-  
-    for (i = 0; i < close.length; i++) {
-      close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-      }
-    }
-  }
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
 
 // Click on a close button to hide the current list item
 var close = document.getElementsByClassName("close");
@@ -44,4 +27,4 @@ for (i = 0; i < close.length; i++) {
     div.style.display = "none";
   }
 }
-
+}
